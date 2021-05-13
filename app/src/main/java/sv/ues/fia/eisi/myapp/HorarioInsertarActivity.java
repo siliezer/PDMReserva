c7669120024b51c76a94bad5cad4be13a8d0c4b8
Coralinav <mcna97@gmail.com>
2021-05-12 21:24:36 -0600

    Cruds de Horario

---
+++ app/src/main/java/sv/ues/fia/eisi/myapp/HorarioInsertarActivity.java
@@ -0,0 +1,14 @@
+package sv.ues.fia.eisi.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HorarioInsertarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horario_insertar);
+    }
+}
---
+++ app/src/main/java/sv/ues/fia/eisi/myapp/HorarioMenuActivity.java
@@ -0,0 +1,14 @@
+package sv.ues.fia.eisi.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HorarioMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horario_menu);
+    }
+}

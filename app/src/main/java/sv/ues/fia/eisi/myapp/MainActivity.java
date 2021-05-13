1b3ccf0f95e1e7da94ea17b65473694ab70b51b0
Coralinav <mcna97@gmail.com>
2021-05-12 21:59:34 -0600

    lista

--- app/src/main/java/sv/ues/fia/eisi/myapp/MainActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/myapp/MainActivity.java
@@ -14,9 +14,9 @@ import android.widget.ListView;
 public class MainActivity extends ListActivity {
 
     String[] menu={"Tabla Escuela","Tabla Ciclo","Tabla Materia","Tabla 4","Tabla 5","Tabla 6","Tabla 7",
-            "Tabla 8","Tabla 9","Tabla 10","Tabla 11","Tabla 12","Tabla 13","Tabla 14",
-            "Tabla 15","Llenar Base de Datos"};
-    String[] activities={"EscuelaMenuActivity","CicloMenuActivity","MateriaMenuActivity"};
+            "Tabla 8","Tabla 9","Tabla 10","Tabla 11","Tabla 12","Tabla Laboratorio","Tabla Horario",
+            "Tabla Propuesta","Llenar Base de Datos"};
+    String[] activities={"EscuelaMenuActivity","CicloMenuActivity","MateriaMenuActivity", "LaboratorioMenuActivity","HorarioMenuActivity","PropuestaMenuActivity"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {

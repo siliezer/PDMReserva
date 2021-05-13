e5ce6223d8a7bbc05e7717c35e7557aa905f5300
Coralinav <mcna97@gmail.com>
2021-05-12 22:18:50 -0600

    sublistas propuesta, horario, lab

--- app/src/main/java/sv/ues/fia/eisi/myapp/PropuestaMenuActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/myapp/PropuestaMenuActivity.java
@@ -1,14 +1,44 @@
 package sv.ues.fia.eisi.myapp;
-
-import androidx.appcompat.app.AppCompatActivity;
-
+//NA15004
+import android.app.ListActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class PropuestaMenuActivity extends AppCompatActivity {
+public class PropuestaMenuActivity extends ListActivity {
+
+    String[] menu = {"Insertar Propuesta","Eliminar Propuesta","Consultar Propuesta",
+            "Actualizar Propuesta"};
+    String[] activities ={"PropuestaInsertarActivity","PropuestaEliminarActivity","PropuestaConsultarActivity",
+            "PropuestaActualizarActivity"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_propuesta_menu);
+        ListView listView = getListView();
+        listView.setBackgroundColor(Color.LTGRAY);
+        ArrayAdapter<String> adapter = new
+                ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, menu);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l,View v,int position,long id) {
+        super.onListItemClick(l, v, position, id);
+
+        String nombreValue = activities[position];
+
+        l.getChildAt(position).setBackgroundColor(Color.DKGRAY);
+
+        try {
+            Class<?> clase = Class.forName("sv.ues.fia.eisi.myapp." + nombreValue);
+            Intent inte = new Intent(this, clase);
+            this.startActivity(inte);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }

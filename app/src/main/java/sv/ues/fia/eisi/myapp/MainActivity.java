e87e4a40a943a331cb4c7fe0838a6e210781b5fd
Coralinav <mcna97@gmail.com>
2021-05-13 21:15:19 -0600

    ordenando arreays en mainactivity

--- app/src/main/java/sv/ues/fia/eisi/myapp/MainActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/myapp/MainActivity.java
@@ -16,7 +16,8 @@ public class MainActivity extends ListActivity {
     String[] menu={"Tabla Escuela","Tabla Ciclo","Tabla Materia","Tabla 4","Tabla 5","Tabla 6","Tabla 7",
             "Tabla 8","Tabla 9","Tabla 10","Tabla 11","Tabla 12","Tabla Laboratorio","Tabla Horario",
             "Tabla Propuesta","Llenar Base de Datos"};
-    String[] activities={"EscuelaMenuActivity","CicloMenuActivity","MateriaMenuActivity", "LaboratorioMenuActivity","HorarioMenuActivity","PropuestaMenuActivity"};
+    String[] activities={"EscuelaMenuActivity","CicloMenuActivity","MateriaMenuActivity","Tabla 4","Tabla 5","Tabla 6","Tabla 7",
+            "Tabla 8","Tabla 9","Tabla 10","Tabla 11","Tabla 12", "LaboratorioMenuActivity","HorarioMenuActivity","PropuestaMenuActivity"};
 
     @Override
     public void onCreate(Bundle savedInstanceState) {

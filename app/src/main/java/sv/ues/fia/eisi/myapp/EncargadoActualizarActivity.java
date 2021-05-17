691812a7c75ded42e4262f0feee7d8affbbe55bb
CarlosG97 <33339766+CarlosG97@users.noreply.github.com>
2021-05-16 23:48:14 -0600

    Asignacion y encargado activitys

--- app/src/main/java/sv/ues/fia/eisi/myapp/EncargadoActualizarActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/myapp/EncargadoActualizarActivity.java
@@ -2,13 +2,47 @@ package sv.ues.fia.eisi.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class EncargadoActualizarActivity extends AppCompatActivity {
+    ControlBD helper;
+    EditText editIdEncargado, editNombresEncargado, editApellidosEncargados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_encargado_actualizar);
+        helper = new ControlBD(this);
+        editIdEncargado = (EditText) findViewById(R.id.editIdEncargado);
+        editNombresEncargado = (EditText) findViewById(R.id.editNombresEncargado);
+        editApellidosEncargados = (EditText) findViewById(R.id.editApellidosEncargado);
+
+    }
+
+    public void actualizarEncargado(View v) throws ParseException {
+        String idEncargado = editIdEncargado.getText().toString();
+        String nombresEncargado = editNombresEncargado.getText().toString();
+        String apellidosEncargado = editApellidosEncargados.getText().toString();
+        Encargado encargado = new Encargado(idEncargado, nombresEncargado, apellidosEncargado);
+
+        helper.abrir();
+        String tosti = helper.actualizar(encargado);
+        helper.cerrar();
+        Toast.makeText(this, tosti, Toast.LENGTH_SHORT).show();
+    }
+
+    public void limpiarTexto(View v){
+        editIdEncargado.setText("");
+        editNombresEncargado.setText("");
+        editApellidosEncargados.setText("");
     }
 }
--- app/src/main/java/sv/ues/fia/eisi/myapp/EncargadoInsertarActivity.java
+++ app/src/main/java/sv/ues/fia/eisi/myapp/EncargadoInsertarActivity.java
@@ -3,12 +3,40 @@ package sv.ues.fia.eisi.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class EncargadoInsertarActivity extends AppCompatActivity {
+    ControlBD helper;
+    EditText editIdEncargado, editNombresEncargado, editApellidosEncargados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_encargado_insertar);
+        setContentView(R.layout.activity_asignacion_insertar);
+        helper = new ControlBD(this);
+        editIdEncargado = (EditText) findViewById(R.id.editIdPropuesta);
+        editNombresEncargado = (EditText) findViewById(R.id.editIdHorario);
+        editApellidosEncargados = (EditText) findViewById(R.id.editIdDia);
+    }
+
+    public void insertarMateria(View v){
+        String regInsertados;
+        String idEncargado = editIdEncargado.getText().toString();
+        String nombresEncargado = editNombresEncargado.getText().toString();
+        String apellidosEncargado = editApellidosEncargados.getText().toString();
+
+        Encargado encargado = new Encargado(idEncargado, nombresEncargado, apellidosEncargado);
+        helper.abrir();
+        regInsertados = helper.insertar(encargado);
+        helper.cerrar();
+        Toast.makeText(this, regInsertados, Toast.LENGTH_SHORT).show();
+    }
+
+    public void limpiarTexto(View v){
+        editIdEncargado.setText("");
+        editNombresEncargado.setText("");
+        editApellidosEncargados.setText("");
     }
 }
